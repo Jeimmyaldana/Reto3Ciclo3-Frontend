16ad349cf8179294d69849081dc51d431ab9d177
Sofía <sofia.neira@ias.com.com>
2022-10-06 09:29:32 -0500

    first commit

---
+++ js/funcionesCategoria.js
@@ -0,0 +1,59 @@
+//GET, POST , PUT Y DELETE
+
+function getCategoria (){
+    $.ajax({
+        url:"http://localhost:8080/api/Category/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            
+            pintarCategoria(respuesta);
+        }
+    });
+
+}
+
+function postCategoria(){
+
+    let cajas = {
+        name:$("#name").val(),
+        description:$("#description").val()
+    };
+    $.ajax({
+        url:"http://localhost:8080/api/Category/save",
+        type:"POST",
+        datatype:"JSON",
+        contentType:"application/json; charset=utf-8",
+        data: JSON.stringify(cajas),
+        success:function(respuesta){
+            alert("se creo correctamente la categoria");
+            window.location.reload();
+    
+        }
+    });
+}
+
+function putCategoria(){
+
+}
+
+function deleteCategoria(){
+
+}
+
+////////////////////////////////////////////
+
+function pintarCategoria(respuesta){
+    console.log(respuesta);
+    let myTable="<table>";
+    for(i=0;i<respuesta.length;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+respuesta[i].name+"</td>";
+        myTable+="<td>"+respuesta[i].description+"</td>";
+        myTable+="</tr>";
+    }
+    myTable+="</table>";
+    $("#resultado1").html(myTable);
+
+
+}
---
+++ js/funcionesReservaciones.js
@@ -0,0 +1,28 @@
+//GET, POST , PUT Y DELETE
+
+function getReservaciones (){
+
+}
+
+function postReservaciones(){
+
+}
+
+function putReservaciones(){
+
+}
+
+function deleteReservaciones(){
+    
+}
+
+///////////////////////////////////////////////
+
+function getClient_Reservation(){
+
+}
+
+function getMachine_Message(){
+
+    
+}
